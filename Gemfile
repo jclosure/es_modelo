43bc7f77097eb9e09bfaaa63d6d0838afa6fb1f7
Joel Holder <jclosure@gmail.com>
2015-09-04 01:18:52 -0500

    added initial code exemplar

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'searchkick'
+gem 'activerecord'
+gem 'mysql2'
+gem 'sqlite3'
+gem 'rspec', :require => 'spec'
+
---
+++ modelo.rb
@@ -0,0 +1,77 @@
+require 'rubygems'
+require 'active_record'
+require 'active_record/base'
+require 'mysql2' # or 'pg' or 'sqlite3'
+require 'sqlite3'
+
+ActiveRecord::Base.default_timezone = :utc
+ActiveRecord::Base.time_zone_aware_attributes = true
+
+## read in db config from yaml file
+#ActiveRecord::Base.configurations = YAML::load(IO.read('config/database.yml'))
+#ActiveRecord::Base.establish_connection("development")
+
+## manual db configurations
+
+## MySql
+# ActiveRecord::Base.establish_connection(
+#   adapter:  'mysql2', # or 'postgresql' or 'sqlite3'
+#   database: 'Test',
+# #  username: 'DB_USER',
+# #  password: 'DB_PASS',
+#   host:     'localhost'
+# )
+
+## Sqlite3
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+
+ActiveRecord::Base.raise_in_transactional_callbacks = true if ActiveRecord::Base.respond_to?(:raise_in_transactional_callbacks=)
+
+## create tables
+
+
+ActiveRecord::Migration.create_table :stores do |t|
+  t.string :name
+end
+
+ActiveRecord::Migration.create_table :products do |t|
+  t.string :name
+  t.integer :store_id
+  t.boolean :in_stock
+  t.boolean :backordered
+  t.integer :orders_count
+  t.decimal :found_rate
+  t.integer :price
+  t.string :color
+  t.decimal :latitude, precision: 10, scale: 7
+  t.decimal :longitude, precision: 10, scale: 7
+  t.text :description
+  t.timestamps null: true
+end
+
+## define models
+
+class Store < ActiveRecord::Base
+  
+end
+
+class Product < ActiveRecord::Base
+  belongs_to :store
+  
+end
+
+ 
+
+## play with models
+
+p = Product.create name: "apples"
+s = Store.create name: "store abc"
+
+p.store = s
+
+p.save
+
+puts p.to_json
+
+puts Product.all.to_json
+
